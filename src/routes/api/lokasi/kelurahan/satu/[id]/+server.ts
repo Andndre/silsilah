c2ec6118d1cbfe874f901a89eda4fa675922565d
andndre <agungandre687@gmail.com>
2024-01-01 10:54:58 +0800

    extend api lokasi

---
+++ src/routes/api/lokasi/kelurahan/satu/[id]/+server.ts
@@ -0,0 +1,18 @@
+import { API_LOKASI_INDONESIA } from '$env/static/private';
+
+export async function GET({ params }) {
+	const test = await fetch(`${API_LOKASI_INDONESIA}/village/${params.id}.json`, {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	return new Response(JSON.stringify(await test.json()), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+			'cache-control': 'public, max-age=60',
+		},
+	});
+}
