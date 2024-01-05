a2ef6f0ba5b54596a7ade167ddcae070bbfd395b
andndre <agungandre687@gmail.com>
2024-01-05 18:37:30 +0800

    warna latar untuk container

--- src/routes/(admin)/grafik-keturunan/rendering.ts
+++ src/routes/(admin)/grafik-keturunan/rendering.ts
@@ -1,8 +1,9 @@
 import type { Circle, ChildNode, Plane2D, RoundedPhoto, RoundedPlane2D, Size2D, Text, ParentNode } from '$lib/types';
 
+const cardWidth = 300;
+const cardHeight = 80;
+
 export function drawChildNode(node: ChildNode, ctx: CanvasRenderingContext2D, drawImage: boolean) {
-	const cardWidth = 300;
-	const cardHeight = 80;
 
 	// Draw rounded card
 	drawRoundedRectagle(
@@ -66,6 +67,14 @@ export function drawChildNode(node: ChildNode, ctx: CanvasRenderingContext2D, dr
 
 export function drawParentNode(node: ParentNode, ctx: CanvasRenderingContext2D, drawImage: boolean) {
 	const padding = 5;
+	drawRoundedRectagle({
+		x: node.x,
+		y: node.y,
+		width: padding * 2 + cardWidth,
+		height: padding * 3 + cardHeight * 2,
+		radius: 14
+	}, ctx, true, 'rgb(200, 200, 200)');
+
 	const ayah = drawChildNode({
 		name: node.namaAyah,
 		birthDate: node.birthDateAyah,
@@ -74,21 +83,13 @@ export function drawParentNode(node: ParentNode, ctx: CanvasRenderingContext2D, 
 		y: node.y + padding
 	}, ctx, drawImage);
 
-	const ibu = drawChildNode({
+	drawChildNode({
 		name: node.namaIbu,
 		birthDate: node.birthDateIbu,
 		photoUrl: node.photoIbu,
 		x: node.x + padding,
 		y: node.y + ayah.height + padding * 2
 	}, ctx, drawImage);
-
-	drawRoundedRectagle({
-		x: node.x,
-		y: node.y,
-		width: padding * 2 + ayah.width,
-		height: padding * 3 + ayah.height * 2,
-		radius: 16
-	}, ctx, false);
 }
 
 export function scatterDots(size: Size2D, ctx: CanvasRenderingContext2D) {
@@ -134,8 +135,8 @@ export function drawCircle(c: Circle, ctx: CanvasRenderingContext2D) {
 	ctx.fill();
 }
 
-export function drawRoundedRectagle(r: RoundedPlane2D, ctx: CanvasRenderingContext2D, fill = true) {
-	ctx.fillStyle = 'white';
+export function drawRoundedRectagle(r: RoundedPlane2D, ctx: CanvasRenderingContext2D, fill = true, color = 'white') {
+	ctx.fillStyle = color;
 	roundedPath(r, ctx);
 	ctx.stroke();
 	if (fill) ctx.fill();
